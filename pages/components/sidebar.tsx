67820c2995b68f0566f272a765d5b9c9b2193e0e
Laura Webber <laura@hightouch.io>
2022-02-06 19:47:57 -0800

    add logo

--- pages/components/sidebar.tsx
+++ pages/components/sidebar.tsx
@@ -1,33 +1,39 @@
-import {
-    Box,
-    List,
-    ListItem,
-    ListIcon,
-    Divider,
-    Center,
-    LinkBox,
-    LinkOverlay
-} from "@chakra-ui/layout"
-import {
-    MdHome,
-    MdSearch,
-    MdLibraryMusic,
-    MdPlaylistAdd,
-    MdFavorite
-} from "react-icons/md"
+/* eslint-disable no-unused-vars */
+import NextImage from "next/image";
 
+import {
+  Box,
+  List,
+  ListItem,
+  ListIcon,
+  Divider,
+  Center,
+  LinkBox,
+  LinkOverlay,
+} from "@chakra-ui/layout";
+import {
+  MdHome,
+  MdSearch,
+  MdLibraryMusic,
+  MdPlaylistAdd,
+  MdFavorite,
+} from "react-icons/md";
 
 const Sidebar = () => {
   return (
-      <Box width="100%" 
-        height="calc(100vh - 100px)"
-        bg="black" 
-        paddingX="5px" 
-        color="gray"
-        >
-
-
+    <Box
+      width="100%"
+      height="calc(100vh - 100px)"
+      bg="black"
+      paddingX="5px"
+      color="gray"
+    >
+      <Box paddingY="20px">
+        <Box width="120px" marginBottom="20px" padding="20px">
+          <NextImage src="/public/logo.svg" height={60} width={120} />
+        </Box>
       </Box>
+    </Box>
   );
 };
 
